1e8f9472a1db0b805fe981564e58bcab76b0750a
Siddhesh2008 <siddheshbala2008@gmail.com>
2026-06-13 00:39:20 +0530

    assign commit

---
+++ assignments/circle.py
@@ -0,0 +1,12 @@
+import math
+radius=float(input("Enter the radius(cm): "))
+circumference=2*math.pi*radius
+area=math.pi*pow(radius,2)
+print(f"the circumference of the circle with radius {radius}\
+ is {round(circumference,2)} cm")
+print(f"the area of th circle is {area} cm^2 ")
+ 
+
+
+
+
---
+++ assignments/hypotneuse.py
@@ -0,0 +1,7 @@
+import math
+a=float(input("side A: "))
+b=float(input("side B: "))
+
+c=math.sqrt(pow(a,2)+pow(b,2))
+
+print(c)
