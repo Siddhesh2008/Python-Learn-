3561b904d9308ac3b1c6ad06bc3a5ced38127624
Siddhesh2008 <siddheshbala2008@gmail.com>
2026-06-13 23:55:05 +0530

    16th commit

---
+++ nested loop.py
@@ -0,0 +1,13 @@
+
+rows=int(input("Enter the # of rows: "))
+columns=int(input("Enter the # of columns: "))
+symbol=input("Enter a symbol: ")
+
+for x in range(rows):
+    for y in range(columns):
+        print(symbol,end="")
+    print()
+
+
+
+
