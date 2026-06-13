69077292f6df25764ce7c846513fc986ec31b15c
Siddhesh2008 <siddheshbala2008@gmail.com>
2026-06-13 14:10:16 +0530

    12th commit

---
+++ while loop.py
@@ -0,0 +1,9 @@
+name=input("Enter your name: ")
+
+while name=="":
+    print("invalid")
+    name=input("Enter your name: ")
+print(f"Hello Mr.{name}")
+
+# while loop can be used with OR ,AND ,NOT logical operators
+
