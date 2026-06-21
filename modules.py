ff63fa6012c9875b7ea64f41809c8610a456e511
Siddhesh2008 <siddheshbala2008@gmail.com>
2026-06-21 12:00:31 +0530

    33rd commit

---
+++ modules.py
@@ -0,0 +1,15 @@
+#module=a file that contains a set of functions you want to include in your application
+#use the import statement to include a module in your code. 
+# This allows you to access the functions and variables defined in that module.
+
+import math  # Importing the built-in math module
+print("This is a module named 'modules.py'.")
+
+print("The square root of 16 is:", math.sqrt(16))  # Using the sqrt function from the math module
+print("The value of pi is:", math.pi)  # Accessing the pi constant from the math module
+
+print("The factorial of 5 is:", math.factorial(5))  # Using the factorial function from the math module
+
+
+
+print(help("modules"))  # Displaying help information about modules in Python
