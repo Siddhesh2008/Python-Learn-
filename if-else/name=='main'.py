7d7269f5db020f6f9a0479b2ee61f12e23bd548f
Siddhesh2008 <siddheshbala2008@gmail.com>
2026-06-21 12:24:59 +0530

    35th commit

---
+++ if-else/name=='main'.py
@@ -0,0 +1,11 @@
+#if __name__ == '__main__': (this script can  be imported or run standalone)
+#                           functions and classes in this model can be reused 
+#                           without the main block being executed.\
+
+def main():
+    #your main code here
+    pass
+
+
+if __name__ == '__main__':
+    main()
