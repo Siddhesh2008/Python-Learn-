d5add19ca72989c3476d544fbcf732afa9791ce7
Siddhesh2008 <siddheshbala2008@gmail.com>
2026-06-24 11:56:52 +0530

    59th commit

--- operator.py
+++
@@ -1,4 +0,0 @@
-a=10        #assigning a value
-b=20
-c=a+b   
-print(c)    #here + is an operator 
---
+++ pyqt5/intro.py
@@ -0,0 +1,20 @@
+#PyQt5 intro
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Cool GUI")
+        self.setGeometry(700,300,700,700)
+
+def main():
+    app=QApplication(sys.argv)
+    window=MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__=="__main__":
+    main()
+
