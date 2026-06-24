90bc50c25e53b8c700e29c74918d65e91836347f
Siddhesh2008 <siddheshbala2008@gmail.com>
2026-06-24 16:59:36 +0530

    65th commit

---
+++ pyqt5/radio.py
@@ -0,0 +1,56 @@
+#PyQt5 radio buttons
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow,QRadioButton,QButtonGroup
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.radio1=QRadioButton("Visa",self)
+        self.radio2=QRadioButton("MasterCard",self)
+        self.radio3=QRadioButton("American Express",self)
+        self.radio4=QRadioButton("In-Store",self)
+        self.radio5=QRadioButton("Online",self)
+        self.button_group1=QButtonGroup(self)
+        self.button_group2=QButtonGroup(self)
+        self.initUI()
+        
+       
+        self.setGeometry(700,300,700,700)
+
+    def initUI(self):
+        self.radio1.setGeometry(0,0,300,50)
+        self.radio2.setGeometry(0,50,300,50)
+        self.radio3.setGeometry(0,100,300,50)
+        self.radio4.setGeometry(0,150,300,50)
+        self.radio5.setGeometry(0,200,300,50)
+
+        self.setStyleSheet("QRadioButton{""font-size:30px;"
+                           "font-family:Arial;"
+                           "color:blue;"
+                           "}")
+        
+        self.button_group1.addButton(self.radio1)
+        self.button_group1.addButton(self.radio2)
+        self.button_group1.addButton(self.radio3)
+        
+        self.button_group2.addButton(self.radio4)
+        self.button_group2.addButton(self.radio5)
+
+        self.radio1.toggled.connect(self.radio_button_changed)
+        self.radio2.toggled.connect(self.radio_button_changed)
+        self.radio3.toggled.connect(self.radio_button_changed)
+        self.radio4.toggled.connect(self.radio_button_changed)
+        self.radio5.toggled.connect(self.radio_button_changed)
+
+    def radio_button_changed(self,checked):
+         radio_button=self.sender()
+         if radio_button.isChecked():
+            print(f"{radio_button.text()} is checked")
+
+
+if __name__=="__main__":
+    app=QApplication(sys.argv)
+    window=MainWindow()
+    window.show()
+    sys.exit(app.exec_())
